116cf5f7d8bc1068055ebef4a857643bcd54e634
Shahwaiz Saleem <shahwaizsaleem8@gmail.com>
2025-09-04 19:52:31 +0500

    check

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -7,7 +7,12 @@ import { Box } from "@mui/material";
 
 export default function Dashboard() {
   return (
-    <Box sx={{ width: { xs: "100%", md: "(100% - 260px)" },  mt:4, }}  >
+    <Box 
+      sx={{ 
+        width: { xs: "100%", md: "calc(100%)" }, // Set width to 100% on small screens, and calc on medium and up
+        mt: 4, 
+      }}
+    >
       <Card />
       <Hero />
       <Chart />
